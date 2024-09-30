f13f7f3fec63d3fcc14c2eff1c4e365917bb1f3d
CHAE_WON_SHIN <96687276+chae-won-shin@users.noreply.github.com>
2024-09-30 19:46:00 +0900

    [Silver I] Title: 음하철도 구구팔, Time: 88 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/1393. 음하철도 구구팔/음하철도 구구팔.js
+++ 백준/Silver/1393. 음하철도 구구팔/음하철도 구구팔.js
@@ -3,16 +3,15 @@ const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 const [xs, ys] = input[0].split(" ").map((value) => +value);
 let [xe, ye, dx, dy] = input[1].split(" ").map((value) => +value);
-let gcf = 0;
 
+let gcf = 0;
 if (dx !== 0 && dy !== 0) {
   for (let i = 1; i <= Math.min(Math.abs(dx), Math.abs(dy)); i++) {
-    if (Math.abs(dx) % i === 0 && Math.abs(dy) % i === 0) {
-      gcf = i;
-    }
+    if (Math.abs(dx) % i === 0 && Math.abs(dy) % i === 0) gcf = i;
   }
-  dx = dx / gcf;
-  dy = dy / gcf;
+
+  dx /= gcf;
+  dy /= gcf;
 }
 
 if (dx === 0) {
@@ -25,9 +24,9 @@ if (dy === 0) {
 
 let answer = Infinity;
 
-while (1) {
-  let dist = Math.pow(xs - xe, 2) + Math.pow(ys - ye, 2);
-  if (answer > dist) answer = dist;
+while (true) {
+  let dist = (xs - xe) ** 2 + (ys - ye) ** 2;
+  if (dist < answer) answer = dist;
   else break;
 
   xe += dx;
