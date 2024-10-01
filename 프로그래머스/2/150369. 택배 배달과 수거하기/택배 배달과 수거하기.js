609b5dbb971f93c976edea4bc3f4b95d3fb4cd49
CHAE_WON_SHIN <96687276+chae-won-shin@users.noreply.github.com>
2024-10-02 01:34:18 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 11.79 ms, Memory: 39.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -0,0 +1,23 @@
+function solution(cap, n, deliveries, pickups) {
+    let answer = 0;
+    let d = 0;
+    let p = 0;
+    
+    for(let i = n-1; i>=0; i--){
+        let cnt = 0;
+        
+        d -= deliveries[i];
+        p -= pickups[i];
+        
+        while (d < 0 || p < 0) {
+            d += cap;
+            p += cap;
+            cnt++;
+        }
+        
+        answer += (i + 1) * 2 * cnt;
+    }
+
+    return answer;
+}
+
