06cfbb6a6b87139cfe96ea97a287aa13ad43581e
CHAE_WON_SHIN <96687276+chae-won-shin@users.noreply.github.com>
2024-10-01 22:41:06 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.75 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,46 +1,43 @@
 function solution(today, terms, privacies) {
-    const [today_year, today_month, today_day] = today.split(".").map(v=>+v);
+    const [t_year, t_month, t_day] = today.split(".").map(v => +v);
     
-    const appoints = {}
-    terms.forEach((term)=>{
-        const [key, value] = term.split(" ");
-        appoints[key] = parseInt(value);
+    const durations = {}
+    terms.forEach((term) => {
+        const [key, duration] = term.split(" ");
+        durations[key] = parseInt(duration);
     })
     
-   const answers = []
-   privacies.forEach((privacy, index)=>{
-       const [collect, key] = privacy.split(" ");
-       const [collect_year, collect_month, collect_day] = collect.split(".").map(v=>+v);
+    const answers = [];
+    privacies.forEach((privacy, index) => {
+        const [collectDate, key] = privacy.split(" ");
+        const [c_year, c_month, c_day] = collectDate.split(".").map(v => +v);
         
-       let finish_day = collect_day - 1;
-       let finish_month = collect_month + appoints[key];
-       let finish_year = collect_year;
-       
-       if (finish_month > 12) {
-           finish_year += Math.floor(finish_month / 12);
-           if(finish_month % 12 == 0) {
-               finish_year -= 1;
-               finish_month = 12;
-           }
-           else finish_month = finish_month % 12;
-       }
-      
-       console.log(finish_year, finish_month, finish_day)
-       
-       if (finish_year < today_year){ 
-           answers.push(index+1)}
-       else if (finish_year === today_year) {
-           if(finish_month < today_month) {
-               answers.push(index+1)
-           }
-           else if (finish_month === today_month) {
-               if (finish_day < today_day) {
-                   answers.push(index+1);
-               }
-           }
-       }
-
-   })
+        let f_year = c_year;
+        let f_month = c_month + durations[key];
+        let f_day = c_day - 1;
+        
+        if(f_month > 12) {
+            f_year += Math.floor(f_month/12);
+            if(f_month % 12 === 0) {
+                f_year -= 1;
+                f_month = 12;
+            }
+            else f_month %= 12;
+        }
+        
+        if (f_year < t_year) {
+            answers.push(index+1);
+        } 
+        else if (f_year === t_year) {
+            if (f_month < t_month) {
+                answers.push(index+1);
+            } 
+            else if (f_month === t_month) {
+                if (f_day < t_day)
+                    answers.push(index+1);
+            }
+        }
+    })
     
     return answers;
 }
