07e9ab05293d16f41533e8a200af3bf8fc01a0fb
CHAE_WON_SHIN <96687276+chae-won-shin@users.noreply.github.com>
2024-10-22 15:23:12 +0900

    [Gold V] Title: 틱택토, Time: 92 ms, Memory: 9536 KB -BaekjoonHub

--- 백준/Gold/7682. 틱택토/틱택토.js
+++ 백준/Gold/7682. 틱택토/틱택토.js
@@ -1,74 +1,81 @@
-const readline = require('readline');
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((e) => e.split(""))
+  .slice(0, -1);
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const board = Array.from({ length: 3 }, () => Array(3));
 
-let board = [];
+const isConnected = (letter) => {
+  // 가로
+  for (let i = 0; i < 3; i++) {
+    if (
+      board[i][0] === letter &&
+      board[i][1] === letter &&
+      board[i][2] === letter
+    )
+      return true;
+  }
 
-const check = (tiktakto) => {
-    // 가로 체크
-    for (let i = 0; i < 3; i++) {
-        if (board[i][0] === tiktakto && board[i][1] === tiktakto && board[i][2] === tiktakto) {
-            return true;
-        }
-    }
-    // 세로 체크
+  // 세로
+  for (let j = 0; j < 3; j++) {
+    if (
+      board[0][j] === letter &&
+      board[1][j] === letter &&
+      board[2][j] === letter
+    )
+      return true;
+  }
+
+  // 대각선
+  if (
+    board[0][0] === letter &&
+    board[1][1] === letter &&
+    board[2][2] === letter
+  )
+    return true;
+  if (
+    board[0][2] === letter &&
+    board[1][1] === letter &&
+    board[2][0] === letter
+  )
+    return true;
+
+  return false;
+};
+
+input.forEach((game) => {
+  const results = [];
+
+  let xCnt = 0;
+  let oCnt = 0;
+
+  // 게임판 만들기
+  for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-        if (board[0][j] === tiktakto && board[1][j] === tiktakto && board[2][j] === tiktakto) {
-            return true;
-        }
+      board[i][j] = game[3 * i + j];
+
+      if (board[i][j] === "X") xCnt++;
+      else if (board[i][j] === "O") oCnt++;
     }
-    // 대각선 체크
-    if (board[0][0] === tiktakto && board[1][1] === tiktakto && board[2][2] === tiktakto) {
-        return true;
-    }
-    if (board[0][2] === tiktakto && board[1][1] === tiktakto && board[2][0] === tiktakto) {
-        return true;
-    }
+  }
 
-    return false;
-};
+  // 게임판이 꽉 찬 경우: X = O+1
+  if (oCnt + xCnt === 9 && xCnt === oCnt + 1) {
+    if (isConnected("X") && isConnected("O")) results.push("invalid");
+    else if (isConnected("O")) results.push("invalid");
+    else results.push("valid");
+  }
+  // 게임판이 꽉 차지 않고 중간에 끝난 경우
+  else {
+    if (isConnected("X") && isConnected("O")) results.push("invalid");
+    else if (isConnected("X") && xCnt === oCnt + 1) results.push("valid");
+    else if (isConnected("O") && xCnt === oCnt) results.push("valid");
+    else results.push("invalid");
+  }
 
-const processInput = (input) => {
-    let results = [];
-
-    for (let s of input) {
-        if (s === "end") break;
-
-        let xCnt = 0;
-        let oCnt = 0;
-        board = Array.from({ length: 3 }, () => Array(3));
-
-        for (let i = 0; i < 3; i++) {
-            for (let j = 0; j < 3; j++) {
-                board[i][j] = s[3 * i + j];
-
-                if (board[i][j] === 'X') xCnt++;
-                else if (board[i][j] === 'O') oCnt++;
-            }
-        }
-
-        // 게임판이 꽉 채워졌을 때
-        if (oCnt + xCnt === 9 && xCnt - oCnt === 1) {
-            if (check('X') && check('O')) results.push("invalid");
-            else if (check('O')) results.push("invalid");
-            else results.push("valid");
-        } else {
-            if (check('X') && check('O')) results.push("invalid");
-            else if (check('X') && xCnt - oCnt === 1) results.push("valid");
-            else if (check('O') && xCnt === oCnt) results.push("valid");
-            else results.push("invalid");
-        }
-    }
-
-    console.log(results.join("\n"));
-};
-
-const inputs = [];
-rl.on('line', (line) => {
-    inputs.push(line);
-}).on('close', () => {
-    processInput(inputs);
+  console.log(results.join("\n"));
 });
