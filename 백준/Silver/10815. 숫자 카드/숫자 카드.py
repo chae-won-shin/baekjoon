222f5a3550c0d9e8b9f1ce9c7acb44b574b54fc6
CHAE_WON_SHIN <96687276+chae-won-shin@users.noreply.github.com>
2025-01-22 23:50:12 +0900

    [Silver V] Title: 숫자 카드, Time: 524 ms, Memory: 378180 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,18 @@
+import sys
+
+card_list = [0]*20000002
+
+n = int(sys.stdin.readline().rstrip())
+cards = list(map(int, sys.stdin.readline().rstrip().split()))
+for card in cards:
+    card_list[card] += 1
+
+m = int(sys.stdin.readline().rstrip())
+seek = list(map(int, sys.stdin.readline().rstrip().split()))
+
+for item in seek:
+    if card_list[item] != 0:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
+        
