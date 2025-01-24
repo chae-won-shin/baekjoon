2dbc86249139ce44a17fdaef2fce31a2c3dd174a
CHAE_WON_SHIN <96687276+chae-won-shin@users.noreply.github.com>
2025-01-24 20:46:32 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 100 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,18 @@
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    k = int(input())
+
+    dp = [[0]*(k+1) for i in range(n+1)]
+    for i in range(k+1):
+        dp[0][i] = i
+    
+    for i in range(n+1):
+        dp[i][1] = 1
+
+    for i in range(1, n+1):
+        for j in range(2, k+1):
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+    print(dp[n][k])
