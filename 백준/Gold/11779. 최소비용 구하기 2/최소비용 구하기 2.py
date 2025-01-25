a97944300ce469086f9fd81933d4662afde5cc99
CHAE_WON_SHIN <96687276+chae-won-shin@users.noreply.github.com>
2025-01-25 20:24:06 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 244 ms, Memory: 135356 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.py
@@ -0,0 +1,40 @@
+import sys
+import heapq
+input = sys.stdin.readline
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+def dijkstra(start, end):
+    distance = [INF] * (n+1)
+    q = []
+    heapq.heappush(q, (0, start, [start]))
+    distance[start] = 0
+
+    while q:
+        dist, now, stops = heapq.heappop(q)
+
+        if dist > distance[now]:
+            continue
+
+        if now == end:
+            return distance[end], stops
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0], stops + [i[0]]))
+
+depart, dest = map(int, input().split())
+
+dist, stops = dijkstra(depart, dest)
+print(dist)
+print(len(stops))
+print(' '.join(str(stop) for stop in stops))
