3e752baafc88e23887eaa636a1b065cf0a271141
CHAE_WON_SHIN <96687276+chae-won-shin@users.noreply.github.com>
2025-02-04 14:40:18 +0900

    [Silver II] Title: N과 M (12), Time: 160 ms, Memory: 12040 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [n, m] = input[0].split(" ").map(Number);
+const arr = input[1]
+  .split(" ")
+  .map(Number)
+  .sort((a, b) => a - b);
+
+const ans = [];
+const res = new Set();
+
+function dfs(depth) {
+  if (depth === m) {
+    res.add(`${ans.join(" ")}`);
+    return;
+  }
+
+  for (const i of arr) {
+    const len = ans.length;
+    if (len === 0 || i >= ans[len - 1]) {
+      ans.push(i);
+      dfs(depth + 1);
+      ans.pop();
+    }
+  }
+}
+
+dfs(0);
+console.log([...res].join("\n"));
