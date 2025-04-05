c7535c6ef0b5b3daa497e60adceaf4fccca39070
CHAE_WON_SHIN <96687276+chae-won-shin@users.noreply.github.com>
2025-04-06 00:33:01 +0900

    [Gold V] Title: 회장뽑기, Time: 104 ms, Memory: 110972 KB -BaekjoonHub

--- 백준/Gold/2660. 회장뽑기/회장뽑기.py
+++ 백준/Gold/2660. 회장뽑기/회장뽑기.py
@@ -1,48 +1,32 @@
-import heapq
 import sys
 input = sys.stdin.readline
 INF = 1e9
 
 n = int(input())
-graph = [[] for _ in range(n+1)]
-points = [INF] * (n+1)
+dist = [[INF] * (n+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    dist[i][i] = 0
 
 while True:
-    p1, p2 = map(int, input().split())
-    
-    if p1 == -1 and p2 == -1:
+    a, b = map(int, input().split())
+    if a == -1 and b == -1:
         break
-    
-    graph[p1].append(p2)
-    graph[p2].append(p1)
+    dist[a][b] = 1
+    dist[b][a] = 1
 
-def dijkstra(start, distance):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
-    while q:
-        dist, now = heapq.heappop(q)
-
-        if distance[now] < dist:
-            continue
-
-        for i in graph[now]:
-            if dist + 1 < distance[i]:
-                distance[i] = dist + 1
-                heapq.heappush(q, (dist+1, i))
+points = [0] * (n+1)
+min_point = INF
 
 for i in range(1, n+1):
-    distance = [INF] * (n+1)
-    dijkstra(i, distance)
-    distance[0] = -1
-    points[i] = max(distance)
+    points[i] = max(dist[i][1:])
+    min_point = min(min_point, points[i])
 
-min_point = min(points)
-candidates = []
-for i in range(1, n+1):
-    if points[i] == min_point:
-        candidates.append(i)
-
+candidates = [i for i in range(1, n+1) if points[i] == min_point]
 print(min_point, len(candidates))
-print(' '.join(str(c) for c in candidates))
+print(' '.join(map(str, candidates)))
