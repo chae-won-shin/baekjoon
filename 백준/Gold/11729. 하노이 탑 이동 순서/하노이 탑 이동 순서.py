94b7ef864c032d4ce16fd46741ffd5697f726c13
CHAE_WON_SHIN <96687276+chae-won-shin@users.noreply.github.com>
2025-04-11 14:45:00 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 356 ms, Memory: 220252 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,16 +1,16 @@
 n = int(input())
 answer = []
 
-def hanoi(n, start, mid, end):
+def hanoi(n, start, end, mid):
     if n == 1:
         answer.append(f'{start} {end}')
         return
     
-    hanoi(n-1, start, end, mid)
+    hanoi(n-1, start, mid, end)
     answer.append(f'{start} {end}')
-    hanoi(n-1, mid, start, end)
+    hanoi(n-1, mid, end, start)
 
-hanoi(n, 1, 2, 3)
+hanoi(n, 1, 3, 2)
 print(len(answer))
 for a in answer:
     print(a)
